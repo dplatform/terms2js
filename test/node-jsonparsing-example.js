5bedc2e50ad1b067aba0350c4e578c970cb7d9c1
Alberto Attilio Reggiori <alberto@derrickmedia.com>
2013-07-05 03:17:21 +0100

    Fixed getAllTermIdMatching() sub-string matching

--- test/node-jsonparsing-example.js
+++ test/node-jsonparsing-example.js
@@ -25,3 +25,4 @@ console.log ("Roles supported: "+num_roles);
 console.log("Is '4.2' a role code? "+roles.isValid ('4.2'));
 console.log("What is the URI of role code '4.2'? "+roles.getTermUri ('4.2'));
 console.log("What is the role code for 'Actor'? "+roles.getAllTermIdMatching ('Actor'));
+console.log("What are the role codes for any term containing with 'Direct'? "+roles.getAllTermIdMatching ('Direct'));
